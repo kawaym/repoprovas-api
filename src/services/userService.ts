8ed6a043c5de9e2da6ebe2abcb736b0891b2a918
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-04-24 20:42:18 -0300

    feat: adds signin route

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,10 @@
 import * as userRepository from "../repositories/userRepository.js";
 
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import user from "../schemas/user.js";
 
-export async function create(user: userRepository.UserCreateData) {
+export async function create(user: userRepository.UserAuthData) {
   const { email, password } = user;
   const hashedPass = hash(password);
   try {
@@ -16,8 +18,52 @@ export async function create(user: userRepository.UserCreateData) {
     throw "internal_server_error";
   }
 }
+export async function login(user: userRepository.UserAuthData) {
+  const { email, password } = user;
+  try {
+    const user = await userRepository.findByEmail(email);
+    if (!user) {
+      throw "not_found";
+    }
+    const comparison = compare(password, user.password);
+    if (!comparison) {
+      throw "unauthorized";
+    }
+    const token = createToken(user);
+    await userRepository.login(user, token);
+    return token;
+  } catch (e) {
+    throw "internal_server_error";
+  }
+}
+export async function validateSession(token: string) {
+  const pass = process.env.PASS_SECRET;
+
+  try {
+    const dados = jwt.verify(token, pass);
+    const session = await userRepository.validateSession(token);
+    if (!session) {
+      throw "unauthorized";
+    }
+    // const user = await userRepository.findByEmail(dados)
+  } catch (e) {
+    throw "unauthorized";
+  }
+}
 
 function hash(item: string) {
   const hashed = bcrypt.hashSync(item, 10);
   return hashed;
+}
+function compare(plain: string, hashed: string) {
+  const comparison = bcrypt.compareSync(plain, hashed);
+  return comparison;
+}
+function createToken(user: userRepository.UserAuthData) {
+  const pass = process.env.PASS_SECRET;
+  const config = { expiresIn: 60 * 60 * 24 };
+
+  const token = jwt.sign(user, pass, config);
+
+  return token;
 }
